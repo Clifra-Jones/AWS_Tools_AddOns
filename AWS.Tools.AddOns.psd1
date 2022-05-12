8ccf68048a4190f4047604737801b1076a03c963
Clifra-Jones <cliffwilliams44@gmail.com>
2022-05-12 13:59:03 -0400

    Modifications to Get-S3Folder and added Restore-S3Folder and Get-S3RestoreProgress

--- AWS.Tools.AddOns.psd1
+++ AWS.Tools.AddOns.psd1
@@ -72,7 +72,7 @@ RequiredModules = @(
 # NestedModules = @()
 
 # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
-FunctionsToExport = 'Get-S3Folder'
+FunctionsToExport = @('Get-S3Folder','Restore-S3Folder','Get-S3RestoreProgress')
 
 # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
 CmdletsToExport = '*'
--- AWS.Tools.AddOns.psm1
+++ AWS.Tools.AddOns.psm1
@@ -5,7 +5,7 @@ function Get-S3Folder () {
         [String]$Prefix,
         [switch]$Files,
         [switch]$Folders,
-        [Int]$MaxServiceCallHistory
+        [Int]$MaxServiceCallHistory=50
     )
 
     If ($MaxServiceCallHistory) {
@@ -50,4 +50,50 @@ function Get-S3Folder () {
         }
     }
     return $result.ToArray() | Sort-Object -Property Key
+}
+
+function Restore-S3Folder () {
+    Param(
+        [Parameter(Mandatory)]
+        [string]$BucketName,
+        [Parameter(Mandatory)]
+        [string]$Prefix,
+        [Parameter(Mandatory)]
+        [int]$CopyLifetime,
+        [Parameter(Mandatory)]
+        [ValidateSet("Standard", "Expedited","Bulk")]
+        [string]$Tier
+    )
+    #Validate the bucket exists
+    $Bucket = Get-S3Bucket -BucketName $BucketName
+    If (-not $Bucket) {
+        Write-Host "Bucket not found!" -ForegroundColor Red
+        exit
+    }
+    $Prefix += ($Prefix.EndsWith("/")) ? "" : "/"
+
+    $s3Keys = Get-S3Object -BucketName $BucketName -Prefix $Prefix
+    if ($s3Keys) {
+        $s3Keys | Restore-S3Object -CopyLifetimeInDays $CopyLifetime -Tier $Tier | Out-Null
+    }
+} 
+
+Function Get-S3RestoreProgress() {
+    Param(
+        [Parameter(Mandatory)]
+        [String]$BucketName,
+        [Parameter(Mandatory)]
+        [string]$Prefix
+    )
+    #valudate buckey name
+    $Bucket = Get-S3Bucket -BucketName $BucketName
+    If (-not $Bucket) {
+        Write-Host"Bucket not found" -ForegroundColor Red
+        exit
+    }
+    $Prefix += ($Prefix.EndsWith("/")) ? "" : "/"
+
+    $s3Keys = Get-S3Object -BucketName $BucketName -Prefix $Prefix
+
+    $S3Keys |Foreach-Object {$Key = $_.Key; $_ | Get-S3ObjectMetadata | Select-Object @{Name="Key";Expression={$Key}},RestoreInProgress, RestoreExpiration}
 }
